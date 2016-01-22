fb6089e8ab9ed22065c7d950be2a733b37aaad34
AGurel <iletisim@agurel.com>
2016-01-22 17:16:17 +0200

    updated

--- PortalProject.Web/Areas/Admin/Controllers/BannerController.cs
+++ PortalProject.Web/Areas/Admin/Controllers/BannerController.cs
@@ -1,8 +1,11 @@
-﻿using PortalProject.Data.UnitOfWork;
+﻿using PortalProject.Core.Domain.Entity;
+using PortalProject.Core.Enums.Common;
+using PortalProject.Data.UnitOfWork;
 using PortalProject.Service.Banners;
 using PortalProject.Web.Areas.Admin.Models;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -24,8 +27,50 @@ namespace PortalProject.Web.Areas.Admin.Controllers
         {
             BannerModel _bannerModel = new BannerModel();
             _bannerModel.BannerList = _bannerService.GetAll().ToList();
-
+            
             return View(_bannerModel);
+        }
+
+        public ActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult BannerAdd(BannerModel model, HttpPostedFileBase Photo)
+        {
+            if (Photo != null)
+            {
+                var fileName = Path.GetFileNameWithoutExtension(Photo.FileName);
+                var extension = Path.GetExtension(Photo.FileName);
+                var path = Path.Combine(Server.MapPath("~/Content/images/manset"), Guid.NewGuid() + fileName.Replace(" ","").Replace(".","") + extension);
+                Photo.SaveAs(path);
+
+                Banner banner = new Banner
+                {
+                    TitleMain = model.TitleMain,
+                    TitleSub = model.TitleSub,
+                    Photo = fileName,
+                    Url = model.Url,
+                    UrlTarget = model.UrlTarget,
+                    Order = model.Order,
+                    Active = model.Active
+                };
+
+                _bannerService.Insert(banner);
+                _uow.SaveChanges();
+            }
+
+            return RedirectToAction("ListBanner");
+        }
+
+        [HttpPost]
+        public ActionResult BannerDelete(int bannerId)
+        {
+            _bannerService.Delete(bannerId);
+            _uow.SaveChanges();
+
+            return RedirectToAction("ListBanner");
         }
     }
 }
--- PortalProject.Web/Areas/Admin/Models/BannerModel.cs
+++ PortalProject.Web/Areas/Admin/Models/BannerModel.cs
@@ -3,11 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using PortalProject.Core.Domain.Entity;
+using PortalProject.Core.Enums.Common;
 
 namespace PortalProject.Web.Areas.Admin.Models
 {
     public class BannerModel
     {
+        public string TitleMain { get; set; }
+        public string TitleSub { get; set; }
+        public string Photo { get; set; }
+        public string Url { get; set; }
+        public string UrlTarget { get; set; }
+        public State Active { get; set; }
+        public int Order { get; set; }
+
         public List<Banner> BannerList { get; set; }
     }
 }
